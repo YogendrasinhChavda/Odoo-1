17f7d4ab88f319f01844e52a683be3295f55eaba
Ammar Officewala <Ammar.o.serpentcs@gmail.com>
2020-02-20 13:18:36 +0530

    [ADD] Added percentage total in profit losse report.

--- account_reports_extended/models/account_general_ledger.py
+++ account_reports_extended/models/account_general_ledger.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 """Inherited General Ledger Report python file."""
-
+import copy
+import ast
 from odoo import models, fields, api, _
 from odoo.tools.misc import format_date, formatLang
 from datetime import datetime, timedelta
 from odoo.addons.web.controllers.main import clean_action
-from odoo.tools import float_is_zero 
+from odoo.tools import float_is_zero, ustr
+from odoo.tools.safe_eval import safe_eval
+from datetime import datetime
+from odoo.osv import expression
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.pycompat import izip
 
 
 class report_account_general_ledger(models.AbstractModel):
@@ -131,8 +138,12 @@ class AccountReport(models.AbstractModel):
         report = {'name': self._get_report_name(),
                 'summary': report_manager.summary,
                 'company_name': self.env.user.company_id.name,}
-        lines = self._get_lines(options, line_id=line_id)
-
+        is_profit = False;
+        profit_losse_action = self.env.ref('account_reports.account_financial_report_profitandloss0')
+        if profit_losse_action and profit_losse_action.id == self.id:
+            is_profit = True
+        # print("????????????????????//", line_id)
+        lines = self.with_context({'is_profit': is_profit})._get_lines(options, line_id=line_id)
         if options.get('hierarchy'):
             lines = self._create_hierarchy(lines)
         footnotes_to_render = []
@@ -148,12 +159,12 @@ class AccountReport(models.AbstractModel):
                     line['footnote'] = str(number)
                     footnotes_to_render.append({'id': f.id, 'number': number, 'text': f.text})
         rcontext = {'report': report,
-                    'lines': {'columns_header': self.get_header(options), 'lines': lines},
+                    'lines': {'columns_header': self.with_context({'is_profit': is_profit}).get_header(options), 'lines': lines},
                     'options': options,
                     'context': self.env.context,
                     'model': self,
                 }
-        profit_losse_action = self.env.ref('account_reports.account_financial_report_profitandloss0')
+        
         if profit_losse_action and profit_losse_action.id == self.id:
             rcontext.update({
                 'is_total': True
@@ -182,3 +193,253 @@ class AccountReport(models.AbstractModel):
     def get_currency(self, value):
         currency_id = self.env.user.company_id.currency_id
         return formatLang(self.env, value, currency_obj=currency_id)
+
+class ReportAccountFinancialReport(models.Model):
+
+    _inherit = "account.financial.html.report"
+
+    def _get_columns_name(self, options):
+        columns = [{'name': ''}]
+        if self.debit_credit and not options.get('comparison', {}).get('periods', False):
+            columns += [{'name': _('Debit'), 'class': 'number'}, {'name': _('Credit'), 'class': 'number'}]
+        columns += [{'name': self.format_date(options), 'class': 'number'}]
+        if options.get('comparison') and options['comparison'].get('periods'):
+            for period in options['comparison']['periods']:
+                columns += [{'name': period.get('string'), 'class': 'number'}]
+            if options['comparison'].get('number_period') == 1 and not options.get('groups'):
+                columns += [{'name': '%', 'class': 'number'}]
+        if self._context.get('is_profit'):
+            columns += [{'name': '%', 'class': 'number'}]
+            columns += [{'name': 'Total', 'class': 'number'}]
+
+        if options.get('groups', {}).get('ids'):
+            columns_for_groups = []
+            for column in columns[1:]:
+                for ids in options['groups'].get('ids'):
+                    group_column_name = ''
+                    for index, id in enumerate(ids):
+                        column_name = self._get_column_name(id, options['groups']['fields'][index])
+                        group_column_name += ' ' + column_name
+                    columns_for_groups.append({'name': column.get('name') + group_column_name, 'class': 'number'})
+            columns = columns[:1] + columns_for_groups
+
+        return columns
+
+    @api.multi
+    def _get_lines(self, options, line_id=None):
+        line_obj = self.line_ids
+        if line_id:
+            line_obj = self.env['account.financial.html.report.line'].search([('id', '=', line_id)])
+        if options.get('comparison') and options.get('comparison').get('periods'):
+            line_obj = line_obj.with_context(periods=options['comparison']['periods'])
+        if options.get('ir_filters'):
+            line_obj = line_obj.with_context(periods=options.get('ir_filters'))
+
+        currency_table = self._get_currency_table()
+        domain, group_by = self._get_filter_info(options)
+
+        if group_by:
+            options['groups'] = {}
+            options['groups']['fields'] = group_by
+            options['groups']['ids'] = self._get_groups(domain, group_by)
+
+        amount_of_periods = len((options.get('comparison') or {}).get('periods') or []) + 1
+        amount_of_group_ids = len(options.get('groups', {}).get('ids') or []) or 1
+        linesDicts = [[{} for _ in range(0, amount_of_group_ids)] for _ in range(0, amount_of_periods)]
+
+        res = line_obj.with_context(
+            cash_basis=options.get('cash_basis'),
+            filter_domain=domain,
+        )._get_lines(self, currency_table, options, linesDicts)
+        return res
+
+class AccountFinancialReportLine(models.Model):
+
+    _inherit = "account.financial.html.report.line"
+
+    @api.multi
+    def _get_lines(self, financial_report, currency_table, options, linesDicts):
+        final_result_table = []
+        comparison_table = [options.get('date')]
+        comparison_table += options.get('comparison') and options['comparison'].get('periods', []) or []
+        currency_precision = self.env.user.company_id.currency_id.rounding
+
+        # build comparison table
+        # print("???????????????/", self._context)
+        is_profit = False
+        opinic_total = 0
+        if (self._context.get('is_profit')):
+            is_profit = True
+        if (is_profit):
+            if self._context and self._context.get('opinic_total'):
+                opinic_total = float(self._context.get('opinic_total'))
+            j = 0
+            opnic_id = self.search([('code', '=', 'OPINC')])
+            res = []
+            domain_ids = {'line'}
+            debit_credit = len(comparison_table) == 1
+            for period in comparison_table:
+                    date_from = period.get('datee_from', False)
+                    date_to = period.get('date_to', False) or period.get('date', False)
+                    date_from, date_to, strict_range = opnic_id.with_context(date_from=date_from, date_to=date_to)._compute_date_range()
+
+                    r = opnic_id.with_context(
+                        date_from=date_from,
+                        date_to=date_to,
+                        strict_range=strict_range)._eval_formula(
+                            financial_report,
+                            debit_credit,
+                            currency_table,
+                            linesDicts[j],
+                            groups=options.get('groups'))
+                    debit_credit = False
+                    res.extend(r)
+                    for column in r:
+                        domain_ids.update(column)
+                    j += 1
+            res = opnic_id._put_columns_together(res, domain_ids)
+            total = 0
+            for rec in res['line']:
+                    total += rec
+            opinic_total = total
+        for line in self:
+            res = []
+            debit_credit = len(comparison_table) == 1
+            domain_ids = {'line'}
+            k = 0
+            for period in comparison_table:
+                date_from = period.get('datee_from', False)
+                date_to = period.get('date_to', False) or period.get('date', False)
+                date_from, date_to, strict_range = line.with_context(date_from=date_from, date_to=date_to)._compute_date_range()
+
+                r = line.with_context(date_from=date_from,
+                                      date_to=date_to,
+                                      strict_range=strict_range)._eval_formula(financial_report,
+                                                                               debit_credit,
+                                                                               currency_table,
+                                                                               linesDicts[k],
+                                                                               groups=options.get('groups'))
+                debit_credit = False
+                res.extend(r)
+                for column in r:
+                    domain_ids.update(column)
+                k += 1
+
+            res = line._put_columns_together(res, domain_ids)
+
+            if line.hide_if_zero and all([float_is_zero(k, precision_rounding=currency_precision) for k in res['line']]):
+                continue
+
+            total = 0
+            if (is_profit):
+                res['line'].append(0.0)
+                for rec in res['line']:
+                    total += rec
+                res['line'].append(total)
+            vals = {
+                'id': line.id,
+                'name': line.name,
+                'level': line.level,
+                'class': 'o_account_reports_totals_below_sections' if self.env.user.company_id.totals_below_sections else '',
+                'columns': [{'name': l} for l in res['line']],
+                'unfoldable': len(domain_ids) > 1 and line.show_domain != 'always',
+                'unfolded': line.id in options.get('unfolded_lines', []) or line.show_domain == 'always',
+                'page_break': line.print_on_new_page,
+            }
+            # print("??????????????????//", vals)
+            if financial_report.tax_report and line.domain and not line.action_id:
+                vals['caret_options'] = 'tax.report.line'
+
+            if line.action_id:
+                vals['action_id'] = line.action_id.id
+            domain_ids.remove('line')
+            lines = [vals]
+            groupby = line.groupby or 'aml'
+            if line.id in options.get('unfolded_lines', []) or line.show_domain == 'always':
+                if line.groupby:
+                    domain_ids = sorted(list(domain_ids), key=lambda k: line._get_gb_name(k))
+                for domain_id in domain_ids:
+                    name = line._get_gb_name(domain_id)
+                    if not self.env.context.get('print_mode') or not self.env.context.get('no_format'):
+                        name = name[:40] + '...' if name and len(name) >= 45 else name
+                    vals = {
+                        'id': domain_id,
+                        'name': name,
+                        'level': line.level,
+                        'parent_id': line.id,
+                        'columns': [{'name': l} for l in res[domain_id]],
+                        'caret_options': groupby == 'account_id' and 'account.account' or groupby,
+                    }
+                    if line.financial_report_id.name == 'Aged Receivable':
+                        vals['trust'] = self.env['res.partner'].browse([domain_id]).trust
+                    lines.append(vals)
+                if domain_ids and self.env.user.company_id.totals_below_sections:
+                    lines.append({
+                        'id': 'total_' + str(line.id),
+                        'name': _('Total') + ' ' + line.name,
+                        'level': line.level,
+                        'class': 'o_account_reports_domain_total',
+                        'parent_id': line.id,
+                        'columns': copy.deepcopy(lines[0]['columns']),
+                    })
+            # print("????????????????????/", lines)
+            for vals in lines:
+                if not is_profit and len(comparison_table) == 2 and not options.get('groups'):
+                    if (is_profit):
+                        vals['columns'].insert(-2, line._build_cmp(vals['columns'][0]['name'], vals['columns'][1]['name']))
+                    else:
+                        vals['columns'].append(line._build_cmp(vals['columns'][0]['name'], vals['columns'][1]['name']))
+                    for i in [0, 1]:
+                        vals['columns'][i] = line._format(vals['columns'][i])
+                    if (is_profit):
+                        vals['columns'][-1] = line._format(vals['columns'][-1])
+                else:
+                    vals['columns'] = [line._format(v) for v in vals['columns']]
+                if (is_profit):
+                    # print("????????????????/", res['line'])
+                    if line.code in ['TOP', 'OIN', 'NEP', 'INC', 'GRP']:
+                        rec_vals = line._build_cmp_percentage(res['line'][-1], opinic_total, True)
+                    else:
+                        rec_vals = line._build_cmp_percentage(res['line'][-1], opinic_total)
+                    print("????????????????????//////", vals)
+                    if vals.get('columns') and len(vals.get('columns')) > 2:
+                        vals['columns'][-2] = rec_vals
+
+                if not line.formulas:
+                    vals['columns'] = [{'name': ''} for k in vals['columns']]
+            if len(lines) == 1:
+                new_lines = line.children_ids.with_context(opinic_total=opinic_total, is_profit = is_profit)._get_lines(financial_report, currency_table, options, linesDicts)
+                if new_lines and line.formulas:
+                    if self.env.user.company_id.totals_below_sections:
+                        divided_lines = line.with_context(opinic_total=opinic_total, is_profit = is_profit)._divide_line(lines[0])
+                        result = [divided_lines[0]] + new_lines + [divided_lines[-1]]
+                    else:
+                        result = [lines[0]] + new_lines
+                else:
+                    result = lines + new_lines
+            else:
+                result = lines
+            final_result_table += result
+
+        return final_result_table
+
+    def _build_cmp_percentage(self, balance, comp, is_true=False):
+        # print("????????????????????//balance/", balance)
+        # print("????????????????????//balance/compcompcomp", comp)
+        # print("????????????linelineeeeeeeeeeeeeeeeeeeeeeee????????//balance/compcompcomp", comp)
+        if comp != 0 and is_true:
+            # print("??????_build_cmp_percentage??????????", balance, comp)
+            res = round(balance / comp * 100, 1)
+            # In case the comparison is made on a negative figure, the color should be the other
+            # way around. For example:
+            #                       2018         2017           %
+            # Product Sales      1000.00     -1000.00     -200.0%
+            #
+            # The percentage is negative, which is mathematically correct, but my sales increased
+            # => it should be green, not red!
+            if (res > 0) != (self.green_on_positive and comp > 0):
+                return {'name': str(res) + '%', 'class': 'number color-red'}
+            else:
+                return {'name': str(res) + '%', 'class': 'number color-green'}
+        else:
+            return {'name': _('n/a')}
